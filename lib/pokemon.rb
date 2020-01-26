35aa67a3ea56d3e6b94702ce6fc6e6fb683c85b0
Benjamin Small <ben20262@aim.com>
2020-01-26 00:43:14 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,29 @@
 class Pokemon
+  attr_accessor :name, :type, :db
+  attr_reader :id
+
+  def initialize (id: nil, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save (name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type) VALUES (?, ?)
+    SQL
+    db.execute(sql, name, type)
+  end
+
+  def self.find (id, db)
+    sql = <<-SQL
+      SELECT *
+      FROM pokemon
+      WHERE id = ?
+    SQL
+    array = db.execute(sql, id).first
+    student = self.new(id: array[0], name: array[1], type: array[2], db: db)
+    student
+  end
 end
