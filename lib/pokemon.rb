fecf458598d64d929d79905422967716ed89d196
github username <github email address>
2020-01-29 13:12:57 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -26,13 +26,7 @@ class Pokemon
     #     SQL
     # end
     def self.find(id_num, db)
-        sql = <<-SQL
-             SELECT *
-             FROM pokemon
-             WHERE id = ?
-         SQL
-
-        pokemon_info = db.execute(sql, id_num).flatten
+        pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
         Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
     end
 
