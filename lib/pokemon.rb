dd4c0af53d7fc713957afd3cd15529a5d913f15f
Jacqueline Freeland <secretartist272@gmail.com>
2020-02-28 17:09:31 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,32 @@
 class Pokemon
+    attr_accessor :name, :type, :db
+  attr_reader :id
+
+  def initialize(id: 1, name: "Pikachu", type: "electric", db: @db)
+    @id = id
+    @name =  name
+    @type = type
+    @db = db 
+  end
+
+  def self.save(a_name, a_type, a_db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?)
+    SQL
+
+    a_db.execute(sql, a_name, a_type)
+  end 
+
+  def self.find(a_id, a_db)
+    sql = <<-SQL
+      SELECT * FROM pokemon
+      WHERE id = ?
+      LIMIT 1
+    SQL
+
+    row = a_db.execute(sql, a_id)[0]
+    self.new(id: row[0], name: row[1], type: row[2])
+
+  end
 end
